457e0712096509e861de3863e574ac4ed8a187a3
georgevasilyev <100374459+georgevasilyev@users.noreply.github.com>
2024-10-24 15:40:52 +0300

    Update hemi.sh

--- hemi.sh
+++ hemi.sh
@@ -2,4 +2,4 @@
 
 mkdir /root/hemi
 cd /root/hemi
-curl -L -O https://github.com/hemilabs/heminetwork/releases/download/v0.5.0/heminetwork_v0.5.0_linux_amd64.tar.gz
+#curl -L -O https://github.com/hemilabs/heminetwork/releases/download/v0.5.0/heminetwork_v0.5.0_linux_amd64.tar.gz
